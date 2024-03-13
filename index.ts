7107ae5f3a3358844b68e739e74cbe995ba30bac
Radwan-Albahrani <radwan158@gmail.com>
2024-03-13 18:44:59 +0300

    LINT

--- index.ts
+++ index.ts
@@ -1,30 +1,26 @@
 // imports
 import { serve } from "bun";
 
-import handleCSV from "./controller/handleCSV";
+import handleCSV from "./handlers/handleCSV";
 
 // Define the port
 const PORT = 3000;
 
 // Start the server
 const server = serve({
-  port: PORT,
-  async fetch(request: Request) {
-    const {method} = request;
-    const url = new URL(request.url);
+    port: PORT,
+    async fetch(request: Request) {
+        const { method } = request;
+        const url = new URL(request.url);
 
-    // Receive a CSV file and return it as a JSON
-    if (method === "POST" && url.pathname === "/upload/csv") {
-        return handleCSV(request);
-    }
+        // Receive a CSV file and return it as a JSON
+        if (method === "POST" && url.pathname === "/upload/csv") {
+            return handleCSV(request);
+        }
 
-
-    return new Response("Not Found", {status: 404});
-  },
+        return new Response("Not Found", { status: 404 });
+    },
 });
 
 // Export the server
 export default server;
-
-
-
